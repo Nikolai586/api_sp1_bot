32cd80e07ab4104b22f6f7eef6bb837bd8b2adaa
Николай <n.chistoprudov@yandex.ru>
2020-05-04 22:27:36 +0300

    my bot_1.2

--- homework.py
+++ homework.py
@@ -11,6 +11,7 @@ PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 proxy = os.getenv('proxy')
+#proxy = telegram.utils.request.Request(proxy_url='socks5://97.74.6.64:5589')
 
 
 def parse_homework_status(homework):
@@ -30,7 +31,8 @@ def get_homework_statuses(current_timestamp):
 
 
 def send_message(message):
-    bot = telegram.Bot(token=TELEGRAM_TOKEN, request=proxy)
+    proxy_url = telegram.utils.request.Request(proxy_url=proxy)
+    bot = telegram.Bot(token=TELEGRAM_TOKEN, request=proxy_url)
     return bot.send_message(chat_id=CHAT_ID, text=message)
 
 
@@ -39,6 +41,7 @@ def main():
 
     while True:
         try:
+            send_message('Я проверяю вашу работу')
             new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
                 send_message(parse_homework_status(new_homework.get('homeworks')[0]))
@@ -54,6 +57,3 @@ def main():
 if __name__ == '__main__':
     main()
 
-#current_timestamp = 0 #int(time.time())
-#print(get_homework_statuses(current_timestamp))
-#send_message('куку')
